93e8d2d3ab9bdc562269adf884098a9557c10d8d
NickStafford2 <41924040+NickStafford2@users.noreply.github.com>
2019-01-15 18:07:07 -0500

    fixed application cors

--- app/application.py
+++ app/application.py
@@ -9,6 +9,12 @@ from flask_cors import CORS
 def create_app(app_name='DIALECTICA_API'):  
     app = Flask(app_name)
     app.config.from_object('app.config.BaseConfig')
+
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
     from app.api import api
     app.register_blueprint(api, url_prefix="/api")
+
+
     return app
